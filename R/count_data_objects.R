3b4b685cb3e3735295c53ee52acb3ed0e934b434
Jeanette Clark <jclark@nceas.ucsb.edu>
2021-03-19 11:24:03 -0700

    add function to count total data objects

---
+++ R/count_data_objects.R
@@ -0,0 +1,21 @@
+#' Count number of new data objects
+#'
+#' @param from Start date to count over (chatacter or POSIXct)
+#' @param to End date to count over (character of POSIXct)
+#'
+#' @return Number of new data objects
+count_data_objects <- function(objects, from = as.POSIXct("1899-01-01"), to = as.POSIXct(Sys.Date())) {
+    if (class(from)[1] == "character"){
+        from <- as.POSIXct(from)
+    }
+    if (class(to)[1] == "character"){
+        to <- as.POSIXct(to)
+    }
+
+    object_count <- objects %>%
+        dplyr::filter(.data$formatType == "DATA") %>%
+        dplyr::filter(is.na(.data$obsoletedBy)) %>%
+        dplyr::filter(.data$dateUploaded >= from & .data$dateUploaded <= to)
+
+    return(nrow(object_count))
+}
