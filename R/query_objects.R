5ddb1a600d11d4e87aa9163ca74bceae6d5c677b
Jeanette Clark <jclark@nceas.ucsb.edu>
2021-04-01 13:33:55 -0700

    add the ability to read and save data from a cache
    this dramatically speeds up query_objects (from 30 minutes to a few seconds) if you have a cached copy of the data that is within an age tolerance that you can set

--- R/query_objects.R
+++ R/query_objects.R
@@ -6,28 +6,54 @@
 #' various metrics and plots.
 #'
 #' @param n (integer) Number of rows to return
+#' @param cache_tolerance (integer) Maximum number of days tolerable for age of
+#' cached results. If the cached results are older than the cache tolerance, a
+#' new dataset will be cached and the old cache deleted. To force a cache
+#' refresh, set tolerance to zero.
 #'
 #' @return (data.frame) Result of the SOLR query
 #'
-query_objects <- function(n = 1000000){
-    D1TOKEN <- getOption("dataone_token")
+query_objects <- function(n = 1000000, cache_tolerance = 14){
 
-    cn <- dataone::CNode("PROD")
-    mn <- dataone::getMNode(cn, "urn:node:ARCTIC")
+    # check for token
+    D1TOKEN <- getOption("dataone_token")
 
     if (is.null(D1TOKEN)) {
         stop('No token set')
     }
 
-    cd <- dataone::query(mn, list(q = '*:*',
-                         fl = 'id,formatType,dateUploaded,obsoletes,obsoletedBy,formatId,origin,size',
-                         sort = 'dateUploaded+desc',
-                         rows = as.integer(n)),
-                as = "data.frame")
-
-    if (nrow(cd) >= 1000000) {
-        stop("Solr query for this plot returned its maximum number of rows. Resutls may be truncated. You'll need to adjust the Solr query or this code to make sure results aren't truncated.")
+    # set up cache
+    if (!(dir.exists(rappdirs::user_cache_dir("arcticreport")))){
+        dir.create(rappdirs::user_cache_dir("arcticreport"))
     }
+
+    cfiles <- grep("objects", dir(rappdirs::user_cache_dir("arcticreport"), full.names = TRUE), value = TRUE)
+    cache_age <- Sys.Date() - as.Date(stringr::str_extract(cfiles, "[0-9]{4}-[0-9]{2}-[0-9]{2}"))
+
+    if (cache_age < cache_tolerance){
+        cd <- readRDS(file = cfiles)
+    } else if (cache_age >= cache_tolerance) {
+        cn <- dataone::CNode("PROD")
+        mn <- dataone::getMNode(cn, "urn:node:ARCTIC")
+
+        cd <- dataone::query(mn, list(q = '*:*',
+                                      fl = 'id,formatType,dateUploaded,obsoletes,obsoletedBy,formatId,origin,size',
+                                      sort = 'dateUploaded+desc',
+                                      rows = as.integer(n)),
+                             as = "data.frame")
+
+        if (nrow(cd) >= 1000000) {
+            stop("Solr query for this plot returned its maximum number of rows. Resutls may be truncated. You'll need to adjust the Solr query or this code to make sure results aren't truncated.")
+        }
+
+        # remove old cached file
+        file.remove(cfiles)
+        # write new cache
+        fpath <- file.path(rappdirs::user_cache_dir("arcticreport"), paste0("objects-", Sys.Date(), ".rds"))
+        saveRDS(cd, file = fpath)
+    }
+
+
 
     return(cd)
 }
