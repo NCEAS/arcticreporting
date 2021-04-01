1a36aa8c82b2d91511aa8021d6f4ec60b80971a1
Jeanette Clark <jclark@nceas.ucsb.edu>
2021-04-01 13:41:05 -0700

    add caching feature to query_version_chains

--- R/query_version_chains.R
+++ R/query_version_chains.R
@@ -5,30 +5,54 @@
 #' object.
 #'
 #' @param objects (data.frame) Result from `query_objects`
+#' @param cache_tolerance (integer) Maximum number of days tolerable for age of
+#' cached results. If the cached results are older than the cache tolerance, a
+#' new dataset will be cached and the old cache deleted. To force a cache
+#' refresh, set tolerance to zero.
 #'
 #' @return (data.frame) Metadata objects data.frame with series identifier
 #'
-query_version_chains <- function(objects){
+query_version_chains <- function(objects, cache_tolerance = 14){
 
     if (is.null(getOption("dataone_token"))) {
         stop('No token set')
     }
 
-    cn <- dataone::CNode("PROD")
-    mn <- dataone::getMNode(cn, "urn:node:ARCTIC")
+    # set up cache
+    if (!(dir.exists(rappdirs::user_cache_dir("arcticreport")))){
+        dir.create(rappdirs::user_cache_dir("arcticreport"))
+    }
 
-    meta <- objects[grep("informatics|gmd|xml", objects$formatId), ]
+    cfiles <- grep("versions", dir(rappdirs::user_cache_dir("arcticreport"), full.names = TRUE), value = TRUE)
+    cache_age <- Sys.Date() - as.Date(stringr::str_extract(cfiles, "[0-9]{4}-[0-9]{2}-[0-9]{2}"))
 
-    unobsoleted <- meta[which(is.na(meta$obsoletedBy)), ]
+    if (cache_age < cache_tolerance){
+        meta <- readRDS(file = cfiles)
+    } else if (cache_age >= cache_tolerance) {
 
-    meta_end <- unobsoleted$id
+        cn <- dataone::CNode("PROD")
+        mn <- dataone::getMNode(cn, "urn:node:ARCTIC")
 
-    meta$seriesId <- NA
-    for (i in 1:length(meta_end)){
-        all_vers <- arcticdatautils::get_all_versions(mn, meta_end[i])
-        z <- which(meta$id %in% all_vers)
-        meta$seriesId[z] <- uuid::UUIDgenerate(n = 1)
-        rm(z)
+        meta <- objects[grep("informatics|gmd|xml", objects$formatId), ]
+
+        unobsoleted <- meta[which(is.na(meta$obsoletedBy)), ]
+
+        meta_end <- unobsoleted$id
+
+        meta$seriesId <- NA
+        for (i in 1:length(meta_end)){
+            all_vers <- arcticdatautils::get_all_versions(mn, meta_end[i])
+            z <- which(meta$id %in% all_vers)
+            meta$seriesId[z] <- uuid::UUIDgenerate(n = 1)
+            rm(z)
+        }
+
+        # remove old cached file
+        file.remove(cfiles)
+        # write new cache
+        fpath <- file.path(rappdirs::user_cache_dir("arcticreport"), paste0("meta-versions-", Sys.Date(), ".rds"))
+        saveRDS(meta, file = fpath)
+
     }
     return(meta)
 }
