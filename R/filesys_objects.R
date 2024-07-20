6b7d775450a6af39e13d1ea83ee32f76624afcdd
Matt Jones <gitcode@magisa.org>
2024-07-19 21:47:42 -0700

    Reformatted to standard 4 space indent.

--- R/filesys_objects.R
+++ R/filesys_objects.R
@@ -15,16 +15,16 @@
 #' @importFrom readr read_csv
 #' @import dplyr
 filesys_objects <- function(){
-  
-  # Get current large data sizes, must be run on datateam
-  large_file <- paste0(getwd(), "/adc-large-data.csv")
-  large_file_cmd <- paste0("cd /var/data/10.18739 && find . -maxdepth 1 -type d | grep A2 | xargs -n 1 basename | xargs -I @ -n 1 sh -c \"stat --printf='%n,%Y,' @ && getfattr --only-values -n ceph.dir.rbytes @ && echo -n ',' && getfattr --only-values -n ceph.dir.rentries @ && echo ''\" > ", large_file)
-  processx::run("bash", c("-c", large_file_cmd))
-  large_data <- readr::read_csv("adc-large-data.csv", col_names = c("id","dateUploaded","size","rentries")) %>%
-    mutate(dateUploaded = as.POSIXct(dateUploaded),
-           formatType = "DATA",
-           size = size*1) %>% 
-    arrange(dateUploaded)
-  
-  return(large_data)
+    
+    # Get current large data sizes, must be run on datateam
+    large_file <- paste0(getwd(), "/adc-large-data.csv")
+    large_file_cmd <- paste0("cd /var/data/10.18739 && find . -maxdepth 1 -type d | grep A2 | xargs -n 1 basename | xargs -I @ -n 1 sh -c \"stat --printf='%n,%Y,' @ && getfattr --only-values -n ceph.dir.rbytes @ && echo -n ',' && getfattr --only-values -n ceph.dir.rentries @ && echo ''\" > ", large_file)
+    processx::run("bash", c("-c", large_file_cmd))
+    large_data <- readr::read_csv("adc-large-data.csv", col_names = c("id","dateUploaded","size","rentries")) %>%
+        mutate(dateUploaded = as.POSIXct(dateUploaded),
+               formatType = "DATA",
+               size = size*1) %>% 
+        arrange(dateUploaded)
+    
+    return(large_data)
 }
